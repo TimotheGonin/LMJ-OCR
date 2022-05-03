67ed3bd6ac4e9c86f440d40ffbb734e97ca57f3a
TimotheGonin <gonin.timothe@gmail.com>
2022-05-03 10:35:56 +0200

    partie-2_chapitre-2

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,6 +1,29 @@
+import logo from '../assets/logo.png'
+import '../styles/Banner.css'
+
 function Banner() {
 	const title = 'La maison jungle'
-	return <h1>{title}</h1>
+
+	//inline style
+	// return (
+	// 	<div
+	// 		style={{
+	// 			colr: 'black',
+	// 			textAlign: 'right',
+	// 			padding: 32,
+	// 			borderBottom: 'solid 3px black'
+	// 		}}
+	// 	>
+	// 		<h1>{title}</h1>
+	// 	</div>
+	// )
+
+	return (
+		<div className='lmj-banner'>
+			<img src={logo} alt='la maison jungle' className='lmj-logo' />
+			<h1 className='lmj-title'>{title}</h1>
+		</div>
+	)
 }
 
 export default Banner
