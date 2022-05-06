dd1cb72b822607186bbe54df1d878a186cb3a98f
TimotheGonin <gonin.timothe@gmail.com>
2022-05-06 22:33:39 +0200

    partie-4_chapitre-2

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,6 +1,6 @@
 import { plantList } from '../datas/plantList'
+import PlantItem from './PlantItem'
 import '../styles/ShoppingList.css'
-import SpecialOffer from './SpecialOffer'
 
 function ShoppingList() {
 	const categories = plantList.reduce(
@@ -18,10 +18,13 @@ function ShoppingList() {
 			</ul>
 			<ul className='lmj-plant-list'>
 				{plantList.map((plant) => (
-					<li key={plant.id} className='lmj-plant-item'>
-						{plant.name}
-						{plant.isSpecialOffer && <SpecialOffer/>}
-					</li>
+					<PlantItem
+						key={plant.id}
+						cover={plant.cover}
+						name={plant.name}
+						water={plant.water}
+						light={plant.light}
+					/>
 				))}
 			</ul>
 		</div>
